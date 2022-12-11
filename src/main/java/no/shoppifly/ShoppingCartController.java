321ee1e0fd0418c44bedb0b61b274ec27eb230a3
aasdag <dag_atle@hotmail.com>
2022-12-11 21:28:10 +0100

    Adding TimedConfig.java and adjusting controller class

--- src/main/java/no/shoppifly/ShoppingCartController.java
+++ src/main/java/no/shoppifly/ShoppingCartController.java
@@ -1,16 +1,22 @@
 package no.shoppifly;
 
 import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController()
-public class ShoppingCartController {
+public class ShoppingCartController implements ApplicationListener<ApplicationReadyEvent> {
 
+    private Map<String, Cart> theCart = new HashMap<>();
     private MeterRegistry meterRegistry;
     @Autowired
     private final CartService cartService;
@@ -55,6 +61,13 @@ public class ShoppingCartController {
     @GetMapping(path = "/carts")
     public List<String> getAllCarts() {
         return cartService.getAllsCarts();
+    }
+
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        Gauge.builder("cart_count", theCart,
+                b -> b.values().size()).register(meterRegistry);
     }
 
 
