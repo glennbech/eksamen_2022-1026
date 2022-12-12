9292ace9b0c57307429132207874de4de069ef51
aasdag <dag_atle@hotmail.com>
2022-12-12 09:11:24 +0100

    Adjusting code

--- infra/provider.tf
+++ infra/provider.tf
@@ -12,3 +12,12 @@ terraform {
   }
 
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "test" {
+  bucket = aws_s3_bucket.analyticsbucket.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
